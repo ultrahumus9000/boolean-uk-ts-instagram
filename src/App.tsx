ab5010f1ca3f886e0a13c6a60c3d7d821dbdbd12
Linlin Li <paris1900lll@gmail.com>
2021-07-17 00:19:02 +0100

    task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import "./reset.css";
 import "./index.css";
+import { Header } from "./components/Header";
+import { CreatePost } from "./components/CreatPostSection";
+import { Feeds } from "./components/Feeds";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="main-header">
+        <Header />
+      </header>
+      <main className="wrapper">
+        <section className="create-post-section">
+          <CreatePost />
+        </section>
+        <section className="feed">
+          <Feeds />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
